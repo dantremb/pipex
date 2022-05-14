4563ed2e60184b381dbeda7efe5db18147917e26
dantremb <dany.tremblay@gmail.com>
2022-05-14 12:55:17 -0400

    test

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,13 +6,13 @@
 /*   By: dantremb <dantremb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 09:39:30 by dantremb          #+#    #+#             */
-/*   Updated: 2022/05/14 11:48:50 by dantremb         ###   ########.fr       */
+/*   Updated: 2022/05/14 12:54:04 by dantremb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void get_function(char **envp)
+char *get_path(char **envp)
 {
 	char	*envp_path = NULL;
 	int		i;
@@ -20,28 +20,44 @@ void get_function(char **envp)
 	i = 0;
 	while (envp[i])
 	{
-		if (ft_strnstr(envp[i], "PiTH=", ft_strlen(envp[i])))
+		if (ft_strnstr(envp[i], "PATH=", ft_strlen(envp[i])))
 		{
 			envp_path = ft_strdup(envp[i]);
 			break ;
 		}
 		i++;
 	}
-	if (!envp_path)
-		ft_putstr("\nNOT FOUND");
-	else
+	return (envp_path);
+}
+
+char	*get_cmd_path(char *env_path, char *cmd)
+{
+	char	**fcnt_path;
+	char	*test_path;
+	int		i;
+	
+	i = 0;
+	fcnt_path = ft_split(env_path, ':');
+	while (fcnt_path[i])
 	{
-		ft_putstr(envp_path);
+		test_path = ft_strjoin(fcnt_path[i], cmd);
+		if (access(test_path, F_OK | X_OK) == 0)
+			return(test_path);
+		free (test_path);
+		i++;
 	}
+	return(NULL);
 }
 
 int main (int argc, char **argv, char **envp)
 {
 	char *options[3] = {"tree", "-f", NULL};
-	
+	char *cmd = "/tree";
 
 	if (argc == 3)
-		get_function(envp);
+	{
+		ft_putstr(get_cmd_path(get_path(envp), cmd));
+	}
 	else if (argc == 2)
 		ft_putstr(argv[1]);
 	else
