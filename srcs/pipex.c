12bb00e58671bc49a0b6092eba6c37d1881190c3
dantremb <dany.tremblay@gmail.com>
2022-05-15 17:04:02 -0400

    15 mai 17:04:02

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,40 +6,42 @@
 /*   By: dantremb <dantremb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 09:39:30 by dantremb          #+#    #+#             */
-/*   Updated: 2022/05/15 16:27:39 by dantremb         ###   ########.fr       */
+/*   Updated: 2022/05/15 17:01:45 by dantremb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	child(char **argv, char **envp, int *fd)
+void	command(char *argv, char **envp)
 {
-	int		file1;
 	char	**options;
 	char	*path;
 	
-	options = ft_split(argv[2], 32);
+	options = ft_split(argv, 32);
 	path = get_cmd_path(envp, ft_strjoin("/", options[0]));
+	execve(path, options, envp);
+}
+
+void	child(char **argv, char **envp, int *fd)
+{
+	int		file1;
+	
 	file1 = open(argv[1], O_RDONLY, 0777);
 	dup2(fd[1], STDOUT_FILENO);
 	dup2(file1, STDIN_FILENO);;
 	close(fd[0]);
-	execve(path, options, envp);
+	command(argv[2], envp);
 }
 
 void	parent(char **argv, char **envp, int *fd)
 {
 	int		file2;
-	char	**options;
-	char	*path;
-	
-	options = ft_split(argv[3], 32);
-	path = get_cmd_path(envp, ft_strjoin("/", options[0]));
+
 	file2 = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
 	dup2(fd[0], STDIN_FILENO);
 	dup2(file2, STDOUT_FILENO);
 	close(fd[1]);
-	execve(path, options, envp);
+	command(argv[3], envp);
 }
 
 int main (int argc, char **argv, char **envp)
